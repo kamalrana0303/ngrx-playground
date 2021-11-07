381f30c403d87f144e4eadb4cf32071164e9e37e
kamalmohansinghrana <kamal@onelap.in>
2021-11-07 13:49:34 +0530

    third commit

--- src/app/utils/home-page-description/home-page-description.component.ts
+++ src/app/utils/home-page-description/home-page-description.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+const config = {
+  small: [Breakpoints.Small, Breakpoints.XSmall],
+  medium: [Breakpoints.Medium],
+  large: [Breakpoints.Large, Breakpoints.XLarge],
+};
 @Component({
   selector: 'app-home-page-description',
   templateUrl: './home-page-description.component.html',
   styleUrls: ['./home-page-description.component.scss']
 })
-export class HomePageDescriptionComponent implements OnInit {
+export class HomePageDescriptionComponent implements OnInit , OnDestroy{
 
-  constructor() { }
+  mobileView:boolean;
+  subscription:Subscription
+  updateView = ({ matches }: BreakpointState) => {
+    if (matches) {
+      this.mobileView=true;
+    } else if (!matches ) {
+      this.mobileView=false;
+    }
+  };
 
+  constructor( private observer: BreakpointObserver) { }
   ngOnInit() {
+    this.subscription=this.observer.observe(config.small).subscribe(this.updateView);
   }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  
 
 }
